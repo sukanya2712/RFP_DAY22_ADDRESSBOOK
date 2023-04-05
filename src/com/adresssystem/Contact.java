c85099817a851432bd266859c71e3ee7191b3792
sukanya2712 <sukanyanaik511@gmail.com>
2023-04-06 01:29:26 +0530

    ADDED_uc1

--- src/com/adresssystem/Contact.java
+++ src/com/adresssystem/Contact.java
@@ -1,4 +1,61 @@
 package com.adresssystem;
 
 public class Contact {
+    private String name;
+    private String age;
+    private String phoneNumber;
+    private String city;
+    private String state;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\nContact{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                "}";
+    }
 }
